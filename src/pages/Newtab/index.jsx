2cb19bbbf0bc67f5815e4ae86da1a332e81d05cd
vagrant <vagrant@devvm>
2022-11-02 09:58:17 +0000

    wip

--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -4,6 +4,6 @@ import { render } from 'react-dom';
 import {Newtab, DAYS, WEEKS, MONTHS, YEARS} from './Newtab';
 import './index.css';
 
-render(<Newtab ageLimitYears={76.61} curAgeYears={33.807} gridType={0}/>, window.document.querySelector('#app-container'));
+render(<Newtab ageLimitYears={76.61} curAgeYears={33.807} gridType={3}/>, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
